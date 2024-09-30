b54d11226697ed8b8aaf9f5ee01ccc7b05a650fc
Jawwad Nadeem <153591645+Jawwad-codes@users.noreply.github.com>
2024-09-29 23:04:34 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Infinitescroller from "./components/Infinitescroller";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Infinitescroller />
+    </div>
+  );
+}
---
+++ src/components/Post.jsx
@@ -0,0 +1,37 @@
+import { useCallback, useEffect } from "react";
+export default function Post({ data, setPageNo }) {
+  const incrementPageNo = useCallback(() => {
+    setPageNo((prevPageNo) => prevPageNo + 1);
+  }, [data]);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (param) => {
+        if (param[0].isIntersecting) {
+          observer.unobserve(lastImage);
+          incrementPageNo();
+        }
+      },
+      { threshold: 0.5 }
+    );
+    const lastImage = document.querySelector(".image-post:last-child");
+    if (!lastImage) {
+      return;
+    }
+    observer.observe(lastImage);
+    return () => {
+      if (lastImage) {
+        observer.unobserve(lastImage);
+      }
+      observer.disconnect();
+    };
+  }, [data]);
+  return (
+    <div className="container">
+      {data.map((item, index) => {
+        return (
+          <img className="image-post" key={item.id} src={item.download_url} />
+        );
+      })}
+    </div>
+  );
+}
